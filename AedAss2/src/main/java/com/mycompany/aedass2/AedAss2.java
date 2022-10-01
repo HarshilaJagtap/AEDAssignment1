7b9a5fbde25758b189f2c2b47203f2cdd59927d3
Harshila Jagtap <50403806+HarshilaJagtap@users.noreply.github.com>
2022-09-30 23:10:45 -0400

    Commit 1
    Work started 24th Sept
    Work done : 
        Swing JFrame with proper indentation
        Create Operation, Update Operation, Delete Operation, Reset Operation
    
    Work Remaining :
     Search operation
     Validations
     Photo displaying and storing

---
+++ AedAss2/src/main/java/com/mycompany/aedass2/AedAss2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aedass2;
+
+import com.aed.view.MainFrame;
+
+/**
+ *
+ * @author harshilajagtap
+ */
+public class AedAss2 {
+    public static void main(String[] args){
+        new MainFrame().setVisible(true);
+    }
+}
